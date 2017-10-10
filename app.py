99d4175b2500cfc680f8c7ea3b5be238bcb634f1
Austen Lamacraft <austen.lamacraft@gmail.com>
2017-10-10 14:05:51 +0100

    Added flask_raven

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
 from flask import Flask
+from flask_raven import raven_auth
+
 signin = Flask(__name__)
 
+signin.config.update(
+    SECRET_KEY='super-secret-key'
+)
+
+
 @signin.route('/')
-def hello():
-    return "Hello World!"
+@raven_auth()
+def home():
+    return "Logged in"
+
+if __name__ == '__main__':
+    signin.run(debug=True)
+
+# from signin.flask_raven import raven_auth
+#
+# signin = Flask(__name__)
+#
+# @signin.route('/')
+# def hello():
+#     return "Hello World!"
