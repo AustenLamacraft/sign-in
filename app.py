34cc33505ea5eaca6036737649bfe02bdb2b1921
Austen Lamacraft <austen.lamacraft@gmail.com>
2017-10-10 15:05:12 +0100

    added login

--- app.py
+++ app.py
@@ -1,25 +1,50 @@
-from flask import Flask
-from flask_raven import raven_auth
-
-signin = Flask(__name__)
-
-signin.config.update(
-    SECRET_KEY='super-secret-key'
-)
-
-
-@signin.route('/')
-@raven_auth()
-def home():
-    return "Logged in"
-
-if __name__ == '__main__':
-    signin.run(debug=True)
-
-# from signin.flask_raven import raven_auth
+# from flask import Flask
+# from flask_raven import raven_auth
 #
 # signin = Flask(__name__)
 #
+# signin.config.update(
+#     SECRET_KEY='super-secret-key'
+# )
+#
+#
 # @signin.route('/')
-# def hello():
-#     return "Hello World!"
+# @raven_auth()
+# def home():
+#     return "Logged in"
+#
+# if __name__ == '__main__':
+#     signin.run(debug=True)
+
+# from flask_raven import raven_auth
+
+from flask import Flask, session, redirect, url_for, escape, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        return 'Logged in as %s' % escape(session['username'])
+    return 'You are not logged in'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return '''
+        <form method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+    '''
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+# set the secret key.  keep this really secret:
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
